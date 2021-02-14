8b4de53cf7c74174be63371b6b7045d7a35ccc8c
slawoe <info@slawomirernst.de>
2021-02-14 14:45:23 +0100

    Add transforms for other elements with transitions and change button styles

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const cardContainer = document.querySelector(".cardContainer");
 const card = document.querySelector(".card");
+const sneaker = document.querySelector(".sneaker img");
+const title = document.querySelector("h1");
+const description = document.querySelector("p");
+const sizes = document.querySelector(".sizes");
+const purchase = document.querySelector(".purchase");
 
 cardContainer.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
@@ -9,9 +14,19 @@ cardContainer.addEventListener("mousemove", (e) => {
 
 cardContainer.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
+  sneaker.style.transform = "translateZ(100px) rotateZ(-10deg)";
+  title.style.transform = "translateZ(85px)";
+  description.style.transform = "translateZ(70px)";
+  sizes.style.transform = "translateZ(55px)";
+  purchase.style.transform = "translateZ(40px)";
 });
 
 cardContainer.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  title.style.transform = "translateZ(0px)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
 });
